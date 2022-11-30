3e2afe65b191cf61ff65c981b9d39994a18b83e9
FELIX-MAIN <117909509+FELIX-MAIN@users.noreply.github.com>
2022-11-30 09:29:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // Write your code here!
-document.getElementById("main").remove();
-   
+let node = document.getElementById("main");
+if(node.parentNode) {
+    node.parentNode.removeChild(main);
+}
+let newHeader = document.createElement("h1");
+    newHeader.setAttribute("id", "victory");
+    newHeader.innerHTML = "YOUR - NAME is the champion";
